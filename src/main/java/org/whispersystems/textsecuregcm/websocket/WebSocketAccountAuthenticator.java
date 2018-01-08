9b7e1e2c34ba7588cbb8a9f270262046cbb859fc
Daniel Hintze <daniel@hintze-it.de>
2018-01-08 10:31:39 +0100

    Logging for debugging

--- src/main/java/org/whispersystems/textsecuregcm/websocket/WebSocketAccountAuthenticator.java
+++ src/main/java/org/whispersystems/textsecuregcm/websocket/WebSocketAccountAuthenticator.java
@@ -1,47 +1,53 @@
 package org.whispersystems.textsecuregcm.websocket;
 
-import com.google.common.base.Optional;
-import org.eclipse.jetty.websocket.api.UpgradeRequest;
-import org.whispersystems.textsecuregcm.auth.AccountAuthenticator;
-import org.whispersystems.textsecuregcm.storage.Account;
-import org.whispersystems.textsecuregcm.storage.Device;
-import org.whispersystems.websocket.auth.AuthenticationException;
-import org.whispersystems.websocket.auth.WebSocketAuthenticator;
-
 import java.util.List;
 import java.util.Map;
 
-import io.dropwizard.auth.basic.BasicCredentials;
+import org.eclipse.jetty.websocket.api.UpgradeRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.whispersystems.textsecuregcm.auth.AccountAuthenticator;
+import org.whispersystems.textsecuregcm.storage.Account;
+import org.whispersystems.websocket.auth.AuthenticationException;
+import org.whispersystems.websocket.auth.WebSocketAuthenticator;
 
+import com.google.common.base.Optional;
+
+import io.dropwizard.auth.basic.BasicCredentials;
 
 public class WebSocketAccountAuthenticator implements WebSocketAuthenticator<Account> {
 
-  private final AccountAuthenticator accountAuthenticator;
+	private final AccountAuthenticator accountAuthenticator;
 
-  public WebSocketAccountAuthenticator(AccountAuthenticator accountAuthenticator) {
-    this.accountAuthenticator = accountAuthenticator;
-  }
+	private final Logger logger = LoggerFactory.getLogger(WebSocketAccountAuthenticator.class);
 
-  @Override
-  public Optional<Account> authenticate(UpgradeRequest request) throws AuthenticationException {
-    try {
-      Map<String, List<String>> parameters = request.getParameterMap();
-      List<String>              usernames  = parameters.get("login");
-      List<String>              passwords  = parameters.get("password");
+	public WebSocketAccountAuthenticator(AccountAuthenticator accountAuthenticator) {
+		this.accountAuthenticator = accountAuthenticator;
+	}
 
-      if (usernames == null || usernames.size() == 0 ||
-          passwords == null || passwords.size() == 0)
-      {
-        return Optional.absent();
-      }
+	@Override
+	public Optional<Account> authenticate(UpgradeRequest request) throws AuthenticationException {
+		try {
+			Map<String, List<String>> parameters = request.getParameterMap();
+			List<String> usernames = parameters.get("login");
+			List<String> passwords = parameters.get("password");
 
-      BasicCredentials credentials = new BasicCredentials(usernames.get(0).replace(" ", "+"),
-                                                          passwords.get(0).replace(" ", "+"));
-      
-      return accountAuthenticator.authenticate(credentials);
-    } catch (io.dropwizard.auth.AuthenticationException e) {
-      throw new AuthenticationException(e);
-    }
-  }
+			if (usernames == null || usernames.size() == 0 || passwords == null || passwords.size() == 0) {
+				return Optional.absent();
+			}
+
+			BasicCredentials credentials = new BasicCredentials(usernames.get(0).replace(" ", "+"),
+					passwords.get(0).replace(" ", "+"));
+
+			Optional<Account> authenticate = accountAuthenticator.authenticate(credentials);
+			logger.info("authenticate: " + authenticate.isPresent());
+			if(authenticate.isPresent() ) logger.info("authenticate: " + authenticate.get().getName());
+			
+			return authenticate;
+		} catch (io.dropwizard.auth.AuthenticationException e) {
+			logger.info("authenticate: " + e.getMessage());
+			throw new AuthenticationException(e);
+		}
+	}
 
 }
